844b41a34095a8c03eb37217419fd7b024e134e2
Diana Pribytkova <dianababayanarturovna777@gmail.com>
2023-07-08 10:38:38 +0300

    Driver

---
+++ src/test/java/org/example/App_Order_Pozitive_Test.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class App_Order_Pozitive_Test{
+    private WebDriver driver;
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+    @BeforeEach
+     public void BeforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    public void AfterEach() {
+        driver.quit();
+        driver = null;
+    }
+}
