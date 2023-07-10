a35f699c434935a3cb6ebd67fcf28f485598813b
Diana Pribytkova <dianababayanarturovna777@gmail.com>
2023-07-10 21:49:11 +0300

    fix application port

--- src/test/java/org/example/App_Order_Pozitive_Test.java
+++ src/test/java/org/example/App_Order_Pozitive_Test.java
@@ -26,7 +26,7 @@ public class App_Order_Pozitive_Test{
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver.get("http://localhost:7777");
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
